e911f339efcf999706cac40a965bfc4c9786a63c
Shashwat Bajpai <f20213041@hyderabad.bits-pilani.ac.in>
2024-09-16 20:19:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# Add API keys here for integration with messaging platforms like Telegram or Slack
+TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
+SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")
